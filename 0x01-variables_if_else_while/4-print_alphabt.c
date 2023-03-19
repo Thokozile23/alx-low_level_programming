d5d4df9b5479987f1a7ff37bd1ea08515e942796
Thokozile23 <thokomunthali22@gmail.com>
2023-03-18 17:58:07 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
 #include<string.h>
 
 /**
- * main-Prints the alphabet without q and e.
+ * main -Prints the alphabet without q and e.
  *
  * Return:Always 0(Success)
  */
@@ -11,14 +11,14 @@ int main(void)
 {
 	int i;
 
-	for(i=97;i<123;i++)
+	for (i = 97; i < 123; i++)
 	{
-		if(i!=101 && i!=113)
+		if(i != 101 && i != 113)
 		{
 			putchar(i);
 		}
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
 
